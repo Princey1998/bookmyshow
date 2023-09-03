f192dda1350f998b9587bf17426cb6248806b9af
Prince Kumar Yadav <109223313+Princey1998@users.noreply.github.com>
2023-09-03 13:36:37 +0530

    Update data.js

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -1,3 +1,3 @@
-export let movies = ['Suraj par mangal bhari', 'Tenet', 'The war with grandpa', 'The personal history of David Copperfield', 'Come Play']
+export let movies = ['Ganges of Wasseypur', 'Tenet', 'Jersey', 'Son of sardaar', 'Come Play']
 export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"]
 export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2']
